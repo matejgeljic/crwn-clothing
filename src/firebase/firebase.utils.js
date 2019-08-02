55440f4865cbba09e63d3705318474fbf4bb10b5
matejgeljic <matejgeljic98@gmail.com>
2019-08-02 11:18:33 +0200

    firestore auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,9 +7,35 @@ const config = {
     authDomain: "crwn-db-1e0e4.firebaseapp.com",
     databaseURL: "https://crwn-db-1e0e4.firebaseio.com",
     projectId: "crwn-db-1e0e4",
-    storageBucket: "",
+    storageBucket: "crwn-db-1e0e4.appspot.com",
     messagingSenderId: "268992037499",
     appId: "1:268992037499:web:d57c0f181bcd883c"
+  }
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+      if (!userAuth) return;
+
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+      const snapShot = await userRef.get();
+
+      if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      
+      try {
+          await userRef.set({
+              displayName,
+              email,
+              createdAt,
+              ...additionalData
+          });
+      } catch (error) {
+          console.log(error.message);
+      }
+    }
+
+    return userRef;
   }
 
   firebase.initializeApp(config);
