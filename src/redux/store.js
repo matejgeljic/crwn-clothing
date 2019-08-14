f4e8c68701a5437748f2c15d20ad0b8ed9b41241
matejgeljic <matejgeljic98@gmail.com>
2019-08-14 13:01:26 +0200

    category pages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist'; 
 
 import rootReducer from './root-reducer';
 
 const middlwares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlwares));
+export const store = createStore(rootReducer, applyMiddleware(...middlwares));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
